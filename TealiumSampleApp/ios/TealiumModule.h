a9594a423377811adc7f376d84514a4ed3673020
Christopher Anderberg <chris@christopher-anderberg-macbook.local>
2018-03-12 09:54:04 -0700

    Implement PR suggestions

--- TealiumSampleApp/ios/TealiumModule.h
+++ TealiumSampleApp/ios/TealiumModule.h
@@ -1,11 +1,3 @@
-//
-//  TealiumRNModule.h
-//  do-over
-//
-//  Created by Christopher Anderberg on 1/8/18.
-//  Copyright © 2018 650 Industries, Inc. All rights reserved.
-//
-
 #ifndef TealiumModule_h
 #define TealiumModule_h
 
