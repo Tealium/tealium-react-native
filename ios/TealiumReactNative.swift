98771cd85b27a914d8dd5db07dff70d6fdc960d9
Christina <christina.sund@tealium.com>
2021-02-03 17:20:07 -0700

    Podspec fixes

---
+++ ios/TealiumReactNative.swift
@@ -0,0 +1,205 @@
+//
+//  TealiumReactNative.swift
+//  TealiumReactNative
+//
+//  Created by Christina S on 12/10/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
+import TealiumSwift
+import React
+
+@objc(TealiumReactNative)
+class TealiumReactNative: RCTEventEmitter {
+    
+    static var tealium: Tealium?
+    private static var config: TealiumConfig?
+    
+    static var visitorServiceDelegate: VisitorServiceDelegate = VisitorDelegate()
+    static var consentExpiryCallback: (([Any]) -> Void)?
+    
+    @objc
+    public static var consentStatus: String {
+        get {
+            tealium?.consentManager?.userConsentStatus.rawValue ?? "unknown"
+        }
+    }
+    
+    @objc
+    public static var consentCategories: [String] {
+        get {
+            var converted = [String]()
+            tealium?.consentManager?.userConsentCategories?.forEach {
+                converted.append($0.rawValue)
+            }
+            return converted
+        }
+    }
+    
+    @objc
+    public static var visitorId: String? {
+        get {
+            tealium?.visitorId
+        }
+    }
+    
+    override init() {
+        super.init()
+        EventEmitter.shared.registerEventEmitter(eventEmitter: self)
+    }
+    
+    @objc
+    override static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
+    
+    @objc
+    override func supportedEvents() -> [String] {
+        return EventEmitter.shared.allEvents
+    }
+    
+    @objc
+    public static func initialize(_ config: [String: Any]) {
+        guard let localConfig = tealiumConfig(from: config) else {
+            return
+        }
+        TealiumReactNative.config = localConfig.copy
+        tealium = Tealium(config: localConfig)
+    }
+
+    @objc
+    public static func track(_ dispatch: [String: Any]) {
+        guard let track = dispatchFrom(dispatch) else {
+            return
+        }
+        tealium?.track(track)
+    }
+    
+    @objc
+    public static func terminateInstance() {
+        guard let config = config else {
+            return
+        }
+        TealiumInstanceManager.shared.removeInstance(config: config)
+        tealium = nil
+    }
+
+    @objc
+    public static func addToDataLayer(data: NSDictionary?, expiry: String) {
+        guard let data = data as? [String: Any] else {
+            return
+        }
+        tealium?.dataLayer.add(data: data, expiry: expiryFrom(expiry))
+    }
+
+    @objc
+    public static func removeFromDataLayer(keys: [String]) {
+        tealium?.dataLayer.delete(for: keys)
+    }
+    
+    @objc
+    public static func getFromDataLayer(key: String) -> Any? {
+        tealium?.dataLayer.all[key]
+    }
+
+    @objc
+    public static func deleteFromDataLayer(keys: [String]) {
+        tealium?.dataLayer.delete(for: keys)
+    }
+
+    @objc
+    public static func addRemoteCommand(id: String) {
+        let remoteCommand = RemoteCommand(commandId: id, description: nil) { response in
+            EventEmitter.shared.dispatch(name: TealiumReactConstants.Events.remoteCommand.rawValue,
+                                                 body: response.payload)
+        }
+        tealium?.remoteCommands?.add(remoteCommand)
+    }
+
+    @objc
+    public static func removeRemoteCommand(id: String) {
+        tealium?.remoteCommands?.remove(commandWithId: id)
+    }
+    
+    @objc
+    public static func setConsentStatus(status: String) {
+        if status == TealiumReactConstants.consented {
+            tealium?.consentManager?.userConsentStatus = .consented
+        } else {
+            tealium?.consentManager?.userConsentStatus = .notConsented
+        }
+    }
+        
+    @objc
+    public static func setConsentCategories(categories: [String]) {
+        tealium?.consentManager?.userConsentCategories = TealiumConsentCategories.consentCategoriesStringArrayToEnum(categories)
+    }
+    
+    @objc
+    public static func joinTrace(id: String) {
+        tealium?.joinTrace(id: id)
+    }
+        
+    @objc
+    public static func leaveTrace() {
+        tealium?.leaveTrace()
+    }
+
+    deinit {
+        EventEmitter.shared.allEvents = []
+    }
+    
+}
+
+class VisitorDelegate: VisitorServiceDelegate {
+    public func didUpdate(visitorProfile: TealiumVisitorProfile) {
+        EventEmitter.shared.dispatch(name: TealiumReactConstants.Events.visitorService.rawValue,
+                                     body: convert(visitorProfile))
+    }
+    
+    private func convert(_ visitorProfile: TealiumVisitorProfile) -> [String: Any] {
+        typealias Visitor = TealiumReactConstants.Visitor
+        let visit: [String: Any?] = [
+            Visitor.dates: visitorProfile.currentVisit?.dates,
+            Visitor.booleans: visitorProfile.currentVisit?.booleans,
+            Visitor.arraysOfBooleans: visitorProfile.currentVisit?.arraysOfBooleans,
+            Visitor.numbers: visitorProfile.currentVisit?.numbers,
+            Visitor.arraysOfNumbers: visitorProfile.currentVisit?.arraysOfNumbers,
+            Visitor.tallies: visitorProfile.currentVisit?.tallies,
+            Visitor.strings: visitorProfile.currentVisit?.strings,
+            Visitor.arraysOfStrings: visitorProfile,
+            // Sets cannot be serialized to JSON, so convert to array first
+            Visitor.setsOfStrings: visitorProfile.currentVisit?.setsOfStrings.map({ (stringSet) -> [String: [String]] in
+                var newValue = [String: [String]]()
+                stringSet.forEach {
+                    newValue[$0.key] = Array($0.value)
+                }
+                return newValue
+            })
+        ]
+        let visitor: [String: Any?] = [
+            Visitor.audiences: visitorProfile.audiences,
+            Visitor.badges: visitorProfile.badges,
+            Visitor.dates: visitorProfile.dates,
+            Visitor.booleans: visitorProfile.booleans,
+            Visitor.arraysOfBooleans: visitorProfile.arraysOfBooleans,
+            Visitor.numbers: visitorProfile.numbers,
+            Visitor.arraysOfNumbers: visitorProfile.arraysOfNumbers,
+            Visitor.tallies: visitorProfile.tallies,
+            Visitor.strings: visitorProfile.strings,
+            Visitor.arraysOfStrings: visitorProfile.arraysOfStrings,
+            // Sets cannot be serialized to JSON, so convert to array first
+            Visitor.setsOfStrings: visitorProfile.setsOfStrings.map({ (stringSet) -> [String: [String]] in
+                var newValue = [String: [String]]()
+                stringSet.forEach {
+                    newValue[$0.key] = Array($0.value)
+                }
+                return newValue
+            }),
+            Visitor.currentVisit: visit.compactMapValues { $0 }
+        ]
+        return visitor.compactMapValues({$0})
+    }
+    
+}
--- npm-package/tealium-react-native.podspec
+++ npm-package/tealium-react-native.podspec
@@ -3,23 +3,28 @@ require "json"
 package = JSON.parse(File.read(File.join(__dir__, "package.json")))
 
 Pod::Spec.new do |s|
-  s.name         = "tealium-react-native"
-  s.version      = package["version"]
-  s.summary      = package["description"]
-  s.description  = <<-DESC
+  s.name = "tealium-react-native"
+  s.version = package["version"]
+  s.summary = package["description"]
+  s.description = <<-DESC
                   Tealium React Native Plugin
                    DESC
-  s.homepage     = "https://github.com/tealium/tealium-react-native"
-  s.license    = { :type => "Commercial", :file => "LICENSE.txt" }
-  s.authors      = { "Christina Sund" => "christina.sund@tealium.com", "James Keith" => "james.keith@tealium.com" }
-  s.platforms    = { :ios => "9.0" }
-  s.source       = { :git => "https://github.com/tealium/tealium-react-native.git", :tag => "#{s.version}" }
+  s.homepage = "https://github.com/tealium/tealium-react-native"
+  s.license = { :type => "Commercial", :file => "LICENSE.txt" }
+  s.authors = { "Christina Sund" => "christina.sund@tealium.com", "James Keith" => "james.keith@tealium.com" }
+  s.platforms = { :ios => "9.0" }
+  s.source = { :git => "https://github.com/tealium/tealium-react-native.git", :tag => "#{s.version}" }
 
-  s.source_files = "**/*.{h,c,m,swift}"
-  s.requires_arc = true
+  #s.source_files = "ios/**"
+  #s.exclude_files = "example/**"
+  #s.ios.source_files = "npm-package/ios/*.{h,c,m,swift}"
+  #s.requires_arc = true
+
+  s.platform            = :ios, '10.0'
+  s.source_files        = 'ios/**/*.{m,h,swift}'
+  s.static_framework    = true
 
   s.swift_version = "5.0"
-  s.platform     = :ios, "10.0"
   s.ios.deployment_target = "10.0"
 
   s.user_target_xcconfig = { 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64 i386' }
