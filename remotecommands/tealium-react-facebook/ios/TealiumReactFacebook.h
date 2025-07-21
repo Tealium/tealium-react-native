82f7844ab1c0b0ceb3f745e6124afbb68f594faf
Sebastian <sebastian.krajna@tealium.com>
2025-07-21 15:14:04 +0200

    - Replaced sensitive information in `facebook.json` with placeholder values for security. - Deleted the unused `TealiumReactFacebook.h` header file to clean up the iOS implementation.

--- remotecommands/tealium-react-facebook/ios/TealiumReactFacebook.h
+++
@@ -1,8 +0,0 @@
-// TealiumReactFacebook.h
-
-#import <React/RCTBridgeModule.h>
-
-@interface TealiumReactFacebook : NSObject <RCTBridgeModule>
-@property (nonatomic, weak) RCTBridge *bridge;
-@end
-
